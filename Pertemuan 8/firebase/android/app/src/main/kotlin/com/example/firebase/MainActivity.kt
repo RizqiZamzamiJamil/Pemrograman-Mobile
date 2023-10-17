4d399004eeffc345299a7501db657a6725532528
RizqiZamzamiJamil <rizqizamzamij@gmail.com>
2023-10-17 10:56:28 +0700

    Pertemuan Minggu 8

---
+++ Pertemuan 8/firebase/android/app/src/main/kotlin/com/example/firebase/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.rizam.firebase
+
+import android.os.Bundle;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+class MainActivity: FlutterActivity() {
+}
